0bef1a285db2b95c217bcc2b2eccca2e4c8bd66e
Nick Darlow <dangerous.darlow@gmail.com>
2026-02-10 09:16:36 +0000

    Add header containing icon and title

---
+++ ui/src/components/Header.svelte
@@ -0,0 +1,8 @@
+<script>
+	import { HatGlasses } from 'lucide-svelte';
+</script>
+
+<h1 class="flex gap-2 items-center">
+	<HatGlasses class="h-8 w-8" aria-hidden="true" />
+	<span>spy</span>
+</h1>
--- ui/src/routes/+layout.svelte
+++ ui/src/routes/+layout.svelte
@@ -3,13 +3,23 @@
 	import { locales, localizeHref } from '$lib/paraglide/runtime';
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import Header from '../components/Header.svelte';
 
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
 
-{@render children()}
+<div class="flex flex-col p-4 max-w-screen-2xl mx-auto">
+	<header class="sticky top-0 z-10 mb-4">
+		<Header />
+	</header>
+
+	{@render children()}
+</div>
+
 <div style="display:none">
 	{#each locales as locale}
 		<a href={localizeHref(page.url.pathname, { locale })}>
