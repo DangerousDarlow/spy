0e284e1ebbbdbe1337ddf71981204d77e3c5a688
Nick Darlow <dangerous.darlow@gmail.com>
2026-02-13 08:20:46 +0000

    Update user button styling

--- ui/src/components/UserButton.svelte
+++ ui/src/components/UserButton.svelte
@@ -7,7 +7,7 @@
 
 <button
 	aria-label={m.user_button_aria_label()}
-	class={`flex gap-2 rounded-full bg-primary-500 p-3 text-white ${isToggled && 'rounded-lg'}`}
+	class={`flex gap-2 rounded-full bg-primary-500 p-3 text-gray-50 ${isToggled && 'rounded-lg'}`}
 	on:click={() => {
 		isToggled = !isToggled;
 	}}
