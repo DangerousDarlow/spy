95f9e203d70805197880f7830b81bac36236083a
Nick Darlow <dangerous.darlow@gmail.com>
2026-03-09 09:23:53 +0000

    Add randomise button to Settings

--- ui/src/lib/index.ts
+++ ui/src/lib/index.ts
@@ -1,3 +1,4 @@
 export { APP_NAME } from './constants';
 export { dev } from './dev';
+export { getRandomName } from './getRandomName';
 export { getUuid } from './getUuid';
--- ui/src/settings/Settings.svelte
+++ ui/src/settings/Settings.svelte
@@ -1,7 +1,17 @@
 <script lang="ts">
-	import { dev } from '$lib';
+	import { Copy, Dices } from 'lucide-svelte';
+	import { dev, getUuid, getRandomName } from '$lib';
 	import { m } from '$lib/paraglide/messages.js';
 	import { saveSettingsToLocalStorage, settings } from './settings.svelte.ts';
+
+	async function copyUserIdToClipboard() {
+		await navigator.clipboard.writeText(settings.user.id);
+	}
+
+	function randomiseNameAndId() {
+		settings.user.id = getUuid();
+		settings.user.name = getRandomName();
+	}
 
 	$effect(() => {
 		if (dev) return;
@@ -11,10 +21,21 @@
 
 <div class="settings flex flex-col gap-4">
 	{#if dev}
-		<label class="label">
-			<span class="label-text">{m.settings_id_label()}</span>
-			<input class="input" type="text" bind:value={settings.user.id} />
+		<label for="id" class="label label-text">
+			{m.settings_id_label()}
 		</label>
+
+		<div class="input-group grid-cols-[1fr_auto]">
+			<input id="id" class="input" type="text" readonly value={settings.user.id} />
+			<button
+				id="copy-id"
+				class="ig-button preset-filled-primary-500 px-2"
+				title={m.settings_id_copy()}
+				onclick={copyUserIdToClipboard}
+			>
+				<Copy size={16} />
+			</button>
+		</div>
 	{/if}
 
 	<label class="label">
@@ -26,4 +47,15 @@
 			bind:value={settings.user.name}
 		/>
 	</label>
+
+	{#if dev}
+		<button
+			class="random-button btn preset-filled-primary-500 p-4"
+			aria-label={m.settings_random_button_aria_label()}
+			onclick={randomiseNameAndId}
+		>
+			<Dices size={24} />
+			{m.settings_random_button_text()}
+		</button>
+	{/if}
 </div>
